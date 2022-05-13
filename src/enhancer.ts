a5843386f4106fe59ea96e1776a39c52c8125a28
Anthony Manning-Franklin <anthony@workitspaces.com.au>
2022-05-13 17:54:18 +0800

    fix(enhancer): exclude internal events from id check

--- src/enhancer.ts
+++ src/enhancer.ts
@@ -3,34 +3,36 @@ import {
   PreloadedState,
   Reducer,
   StoreEnhancer,
-  StoreEnhancerStoreCreator
+  StoreEnhancerStoreCreator,
 } from 'redux';
 import { EventStoreBase } from './index';
 
 export const eventStoreReduxEnhancer: StoreEnhancer<any, EventStoreBase> =
   (createStore: StoreEnhancerStoreCreator) =>
-    <S, E extends AnyAction>(reducer: Reducer<S, E>, initialState?: PreloadedState<S>) => {
-
-  const enhancedReducer: Reducer<S, E> = (state, action) => {
-    if (action.id) {
-      /*
+  <S, E extends AnyAction>(
+    reducer: Reducer<S, E>,
+    initialState?: PreloadedState<S>
+  ) => {
+    const enhancedReducer: Reducer<S, E> = (state, action) => {
+      if (action.id) {
+        /*
       Some redux actions may be internal rather than events raised by the user.
       We need to ensure we only update the last event id for events that have an id, and pass thru for redux actions
        */
+        return {
+          ...reducer(state, action),
+          eventStoreMetadata: {
+            lastEventId: action.id,
+          },
+        };
+      }
       return {
         ...reducer(state, action),
         eventStoreMetadata: {
-          lastEventId: action.id,
+          // @ts-ignore
+          lastEventId: state?.eventStoreMetadata?.lastEventId || 0,
         },
       };
-    }
-    return {
-      ...reducer(state, action),
-      eventStoreMetadata: {
-        // @ts-ignore
-        lastEventId: state && state.eventStoreMetadata && state.eventStoreMetadata.lastEventId || 0,
-      }
     };
-  }
-  return createStore(enhancedReducer, initialState);
-}
+    return createStore(enhancedReducer, initialState);
+  };
